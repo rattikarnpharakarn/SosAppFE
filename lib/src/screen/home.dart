c26f95c6050eb608f1a535d9c991bf757ebe2a50
chirapon hemtrakan <chirapon.job@gmail.com>
2023-02-25 23:49:01 +0700

    update homepage

--- lib/src/screen/home.dart
+++ lib/src/screen/home.dart
@@ -104,62 +104,12 @@ class _HomeState extends State<Home> {
           textDirection: TextDirection.ltr,
           children: <Widget>[
             Container(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(1),
-                        child: const Text(
-                          "Good morning Gig.",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            fontSize: 24,
-                            decorationStyle: TextDecorationStyle.solid,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                        child: Text(
-                          'You need any help?',
-                          style: TextStyle(
-                            color: Colors.red.shade800,
-                            fontSize: 18,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.red,
-                            decorationStyle: TextDecorationStyle.solid,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(360),
-                      child: Image.asset(
-                        'assets/images/profile.jpg',
-                        width: 45,
-                        height: 45,
-                        fit: BoxFit.cover,
-                      ),
-                      // Image.memory( base64Decode(imageBase64), fit: BoxFit.cover )
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
               width: 500,
               height: 250,
               padding: const EdgeInsets.fromLTRB(0, 90, 0, 0),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
+                image: const DecorationImage(
                     image: AssetImage("assets/images/home.jpeg"),
                     fit: BoxFit.cover),
               ),
@@ -203,6 +153,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Container(
+              padding: EdgeInsets.zero,
               height: 400,
               width: 400,
               child: GridView.count(
@@ -213,6 +164,7 @@ class _HomeState extends State<Home> {
                 crossAxisCount: 2,
                 children: [
                   Container(
+                    padding: EdgeInsets.zero,
                     child: Card(
                       color: Color.fromRGBO(210, 250, 251, 1),
                       shape: RoundedRectangleBorder(
@@ -222,15 +174,15 @@ class _HomeState extends State<Home> {
                         children: [
                           Container(
                               alignment: Alignment.topLeft,
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 15),
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 15),
                               child: Image.asset(
                                 'assets/images/hospital.png',
                                 height: 100,
                               )),
                           Container(
                               alignment: Alignment.topLeft,
-                              padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                              child: Text(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: const Text(
                                 'โรงพยาบาล',
                                 style: TextStyle(
                                   color: Color.fromRGBO(12, 75, 142, 1),
@@ -242,8 +194,9 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   Container(
+                    padding: EdgeInsets.zero,
                     child: Card(
-                      color: Color.fromRGBO(210, 250, 251, 1),
+                      color: const Color.fromRGBO(210, 250, 251, 1),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(15.0),
                       ),
@@ -259,7 +212,7 @@ class _HomeState extends State<Home> {
                           Container(
                               alignment: Alignment.topLeft,
                               padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                              child: Text(
+                              child: const Text(
                                 'ปอเต็กตึ๊ง',
                                 style: TextStyle(
                                   color: Color.fromRGBO(12, 75, 142, 1),
@@ -271,8 +224,9 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   Container(
+                    padding: EdgeInsets.zero,
                     child: Card(
-                      color: Color.fromRGBO(210, 250, 251, 1),
+                      color: const Color.fromRGBO(210, 250, 251, 1),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(15.0),
                       ),
@@ -280,15 +234,15 @@ class _HomeState extends State<Home> {
                         children: [
                           Container(
                               alignment: Alignment.topLeft,
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 15),
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 15),
                               child: Image.asset(
                                 'assets/images/fire.png',
                                 height: 100,
                               )),
                           Container(
                               alignment: Alignment.topLeft,
-                              padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                              child: Text(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: const Text(
                                 'สถานีดับเพลิง',
                                 style: TextStyle(
                                   color: Color.fromRGBO(12, 75, 142, 1),
@@ -300,8 +254,9 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   Container(
+                    padding: EdgeInsets.zero,
                     child: Card(
-                      color: Color.fromRGBO(210, 250, 251, 1),
+                      color: const Color.fromRGBO(210, 250, 251, 1),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(15.0),
                       ),
@@ -309,21 +264,22 @@ class _HomeState extends State<Home> {
                         children: [
                           Container(
                               alignment: Alignment.topLeft,
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 15),
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 15),
                               child: Image.asset(
                                 'assets/images/other.png',
                                 height: 100,
                               )),
                           Container(
-                              alignment: Alignment.topLeft,
-                              padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                              child: Text(
-                                'สถานีตำรวจ',
-                                style: TextStyle(
-                                  color: Color.fromRGBO(12, 75, 142, 1),
-                                  fontSize: 25,
-                                ),
-                              )),
+                            alignment: Alignment.topLeft,
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: const Text(
+                              'สถานีตำรวจ',
+                              style: TextStyle(
+                                color: Color.fromRGBO(12, 75, 142, 1),
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
