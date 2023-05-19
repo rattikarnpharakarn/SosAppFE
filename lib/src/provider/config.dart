219a026c68c9bbbe5225e31b66c4ec897ec43070
chirapon hemtrakan <chirapon.job@gmail.com>
2023-05-20 03:50:32 +0700

    update

--- lib/src/provider/config.dart
+++ lib/src/provider/config.dart
@@ -1,12 +1,7 @@
-// String urlAccount = 'http://34.143.165.150:80/SosApp/accounts/';
-// String urlEmergency = 'http://34.143.165.150:81/SosApp/emergency/';
-// String urlHotline = 'http://34.143.165.150:82/SosApp/hotline/';
-// String urlMessenger = 'http://34.143.165.150:83/SosApp/messenger/';
-
-String urlAccount = 'http://10.0.2.2:80/SosApp/accounts/';
-String urlEmergency = 'http://10.0.2.2:81/SosApp/emergency/';
-String urlHotline = 'http://10.0.2.2:82/SosApp/hotline/';
-String urlMessenger = 'http://10.0.2.2:83/SosApp/messenger/';
+String urlAccount = 'http://35.229.220.89:80/SosApp/accounts/';
+String urlEmergency = 'http://35.229.220.89:81/SosApp/emergency/';
+String urlHotline = 'http://35.229.220.89:82/SosApp/hotline/';
+String urlMessenger = 'http://35.229.220.89:83/SosApp/messenger/';
 String urlWsMessenger = 'http://10.0.2.2:84/';
 
 
